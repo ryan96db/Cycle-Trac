42c014d593af7f675da9aa051d3b1c3195257daf
Ryan DeBose-Boyd <ryan96db@gmail.com>
2019-01-27 22:35:47 -0600

    Add createWorkout Function, and add Workout entity with name and date.

--- Cycle-Trac/RecentWorkoutsViewController.swift
+++ Cycle-Trac/RecentWorkoutsViewController.swift
@@ -19,6 +19,50 @@ class RecentWorkoutsViewController: UIViewController, UITableViewDelegate, UITab
     }
     
     
+    @IBAction func createNewWorkout(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Add New Workout", message: "Enter a new workout name.", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField: UITextField) -> Void in
+            textField.placeholder = "Enter Workout Name"
+            textField.textAlignment = .center
+            
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Add", style: .default, handler: {
+            alert -> Void in
+            
+            let workoutNameField = alertController.textFields![0] as UITextField
+            
+            if workoutNameField.text != "" {
+                if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                    let newWorkout = Workout(context: context)
+                    
+                    //Unwraps workoutNameField
+                    if let name = workoutNameField.text
+                    {
+                        newWorkout.name = name
+                        
+                        //                performSegue(withIdentifier: "moveToNewWorkoutViewController", sender: workoutItem)
+                        
+                    }
+
+                }
+            }
+            else {
+                let errorAlert = UIAlertController(title: "Error", message: "Please enter a new workout name.", preferredStyle: .alert)
+                errorAlert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: {
+                    alert -> Void in
+                    self.present(alertController, animated: true, completion: nil)
+                }))
+                self.present(errorAlert, animated: true, completion: nil)
+            }
+        }))
+        
+        
+        self.present(alertController, animated: true)
+    }
     
     @IBOutlet weak var recentWorkoutsTableView: UITableView!
     
